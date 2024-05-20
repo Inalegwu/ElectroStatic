a7f998ceabfecb903da6fdb7f70349b08be70fec
Ikwue Inalegwu <ikwueinalegwu@gmail.com>
2024-05-20 21:57:41 +0100

    [chore]cleanup and final testing

--- src/shared/storage.ts
+++ src/shared/storage.ts
@@ -1,11 +1,16 @@
 // these both have to be cjs, 'cause of esm modules
-// in electron
+// in electron. always make sure imports are
+// from /cjs
 import { createStore } from "tinybase/cjs";
 import { createIndexedDbPersister } from "tinybase/cjs/persisters/persister-indexed-db";
 
+// your store/database
 export const store = createStore();
 
+// persist to indexedDB
 const persister = createIndexedDbPersister(store, "__electrostatic_db", 5);
 
+// save the store on load
+// and prepare autosaving
 persister.save();
 persister.startAutoSave();
