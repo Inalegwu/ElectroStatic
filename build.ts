1dde67bcb41009348a625b2741a78f6a7f289ca7
Ikwue Inalegwu <ikwueinalegwu@gmail.com>
2024-05-20 22:18:37 +0100

    migrate to a build config file

---
+++ build.ts
@@ -0,0 +1,37 @@
+import { build } from "electron-builder";
+
+build({
+  config: {
+    appId: "com.electrostatic.app",
+    productName: "ElectroStatic",
+    artifactName: "${product_name}-${version}_${platform}_${arch}.${ext}",
+    buildDependenciesFromSource: true,
+    files: ["out/**/*"],
+    directories: {
+      output: "release/${version}",
+    },
+    mac: {
+      target: ["dmg"],
+    },
+    win: {
+      target: [
+        {
+          target: "msi",
+          arch: ["x64"],
+        },
+      ],
+    },
+    linux: {
+      target: [
+        {
+          target: "AppImage",
+        },
+      ],
+    },
+    msi: {
+      oneClick: true,
+      perMachine: true,
+      runAfterFinish: true,
+    },
+  },
+});
